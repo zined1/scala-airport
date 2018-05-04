8d6dc337801a9108c541275ec418c66251b46924
titrok <antoine.debrenne@epita.fr>
2018-05-05 00:45:07 +0200

    Scala fix
    Try to fix access issues

--- parser.scala
+++ parser.scala
@@ -13,10 +13,16 @@ object main
   def parseLines(l : List[String]): List[List[String]] = l.map{x => csvParserLine(x)}.toList
 
   def getColl(s : String) = MongoClient("localhost", 27017)("Airport")(s)
+  
+  def exists(l : List[String], i : Int) =
+	if (i < l.length)
+		Some(l(i))
+	else
+		None
 
   def main(args: Array[String]): Unit = {
     parseLines(scala.io.Source.fromFile("countries.csv")("UTF-8").mkString.split("\n").toList).foreach { l => 
-    getColl("countries").insert(MongoDBObject("id" -> l(0), "code" -> l(1), "name" -> l(2), "continent" -> l(3), "wikipedia_link" -> l(4), "keywords" -> l(5))) }
+    getColl("countries").insert(MongoDBObject("id" -> exists(l, 0), "code" -> exists(l,1), "name" -> exists(l,2), "continent" -> exists(l,3), "wikipedia_link" -> exists(l,4), "keywords" -> exists(l,5))) }
   }
 }
 //def test(t: String = ""): Any = scala.io.StdIn.readLine() match {
