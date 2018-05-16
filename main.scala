5fd6f2a41434969069912de1deecfffd7d009796
Zinedine <zinedine.rebiai@epita.fr>
2018-05-16 03:41:29 +0200

    Quick meny

--- main.scala
+++ main.scala
@@ -6,12 +6,37 @@ import airport_function._
 object main
 {
 
-  def menu(): Any = scala.io.StdIn.readLine() match {
-    case ("Query") => println("query")
-    case ("Reports") => println("report")
-    case _ => println("Unknown Option")
+  def menu(countriesCollection: MongoCollection, runwaysCollection: MongoCollection, airportsCollection: MongoCollection): Any =
+    {
+      println("1- Query")
+      println("2- Reports")
+      print("-> ")
+      scala.io.StdIn.readLine() match {
+      case "Query" | "1" => query_menu(runwaysCollection, airportsCollection)
+      case "Reports" | "2" => reports_menu(countriesCollection, runwaysCollection, airportsCollection)
+      case _ => println("Unknown Option")
+    }
   }
 
+  def query_menu(runwaysCollection: MongoCollection, airportsCollection: MongoCollection): Any = {
+    println("Enter the country or coutry_code (e.g: France or FR)")
+    print("-> ")
+    query_functions.displayAirportsRunways(airportsCollection, runwaysCollection, scala.io.StdIn.readLine())
+  }
+
+  def reports_menu(countriesCollection: MongoCollection, runwaysCollection: MongoCollection, airportsCollection: MongoCollection): Any =
+    {
+      println("1- Highest/Lowest number of airports")
+      println("2- Type of runways")
+      println("3- 10 most common runway latitude")
+      print("-> ")
+      scala.io.StdIn.readLine() match {
+      case "1" => reports_functions.rankNumberAirports(airportsCollection, countriesCollection)
+      case "2" => reports_functions.typeRunways(airportsCollection, countriesCollection)
+      case "3" => reports_functions.commonRunways(runwaysCollection)
+      case _ => println("Unknown Option")
+      }
+    }
   def main(args: Array[String]): Unit = {
     val mongoClient = MongoClient("localhost", 27017)("Airport")
     val countriesCollection = mongoClient("countries")
@@ -25,14 +50,9 @@ object main
     airportsCollection.createIndex("id")
     insert_collection.insertAirportsCollection(airportsCollection, "airports.csv")
 
-    val input = "France"
-    val list = countriesCollection.find(MongoDBObject("name" -> input)).toList
-    val countryCode = if (!list.isEmpty) list(0)("code") else (input)
-
-    query_functions.displayAirportsRunways(airportsCollection, runwaysCollection, countryCode)
-
-    reports_functions.rankNumberAirports(airportsCollection, countriesCollection)
-    reports_functions.typeRunways(airportsCollection, countriesCollection)
-    reports_functions.commonRunways(runwaysCollection)
+    //val input = "France"
+    //val list = countriesCollection.find(MongoDBObject("name" -> input)).toList
+    //val countryCode = if (!list.isEmpty) list(0)("code") else (input)
+    menu(countriesCollection, runwaysCollection, airportsCollection)
   }
 }
